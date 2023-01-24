9b47f49fef482f73e32d12276539536f7402ca7f
Joe <jweathingtonsr@gmail.com>
2023-01-23 21:23:02 -0600

    navbar margins and spacing updated

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,24 +7,23 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       {user ?
-        <div class="container-fluid" className={styles.container}>
+        <div class="container-fluid">
           <Link to="/" className={styles.techcen}>TECH CENTER</Link>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div className={styles.search}></div>
+          
+          
+          <div class="collapse navbar-collapse" id="navbarSupportedContent" className={styles.rightnav}>
+          <div className={styles.search}>
           <form class="d-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button class="btn btn-outline-success" type="submit">Search</button>
           </form>
-          
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          </div>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
               <li class="nav-item">
               </li>
-              <li class="nav-item dropdown">
-                <Link to="/account" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Account</Link>
+              <li class="nav-item dropdown" className={styles.accountdrop}>
+                <Link to="" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Account</Link>
                 <ul class="dropdown-menu">
                   <li><a class="dropdown-item" href="#">Action</a></li>
                   <li><a class="dropdown-item" href="#">Another action</a></li>
@@ -42,9 +41,9 @@ const NavBar = ({ user, handleLogout }) => {
 
 
         :
-        <ul>
-          <li><Link to="/login">Log In</Link></li>
-          <li><Link to="/signup">Sign Up</Link></li>
+        <ul className={styles.loginandsignup}>
+          <li className={styles.login}><Link to="/login">Log In</Link></li>
+          <li className={styles.signup}><Link to="/signup">Sign Up</Link></li>
         </ul>
       }
     </nav>
